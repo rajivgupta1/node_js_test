38accd8da8b3c5651069060dc4a7e7277fb1c6c2
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-06-04 13:19:14 +1000

    hello again

--- index.js
+++ index.js
@@ -1,77 +1,86 @@
-import express from "express"
-
-const app= express()
-
+import express from "express";
+const app = express();
 const PORT = 8000;
 
 import path from "path";
-import fs from "fs"
+import fs from "fs";
 
 const __dirname = path.resolve();
 const fn = __dirname + "/userList.csv";
 
-
-console.log(__dirname);
-
-
-//middleware
-app.use(express.urlencoded ());
+// middleware
+app.use(express.urlencoded());
 
 // routers
-app.get("/register", (req, res) =>{
-    console.log(req.query);
+app.get("/register", (req, res) => {
+  console.log(req.query);
 
-    // res.send("<h1> You are in the registration</h1>");
-     res.sendFile(__dirname + "/src/regForm.html");
+  //   res.send("<h1>You are in the registration</h1>");
+
+  res.sendFile(__dirname + "/src/regForm.html");
 });
 
-app.post("/register", (req, res) =>{
-    console.log(req.body);
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  const str = `${email},${password}\n`;
+  fs.appendFile(fn, str, (error) => {
+    error ? console.log(error.message) : console.log("added to the file");
+  });
 
-const {email, password} = req.body;
-const str = `${email}, ${password} \n`;
+  //   res.send("<h1>You are registered, you may login in now</h1>");
 
-fs.appendFile(fn, str, (error) =>{
-    error? console.log(error.message) : console.log('added to the file');
-});
- 
-    // res.send("<h1> You are in the register and u may login now</h1>");
-    res.sendFile(__dirname + "/src/regForm.html");
-
+  res.sendFile(__dirname + "/src/regForm.html");
 });
 
-
-
-
-
-// login
-app.get("/login", (req, res) =>{
-    console.log("recived request to home login");
-
-    res.send("<h1> You are in the login");
-
-    
+app.get("/login", (req, res) => {
+  //   res.send("<h1>You are in the Login</h1>");
+  res.sendFile(__dirname + "/src/loginForm.html");
 });
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const str = `${email},${password}`;
 
+  //read file
+  fs.readFile(fn, (error, data) => {
+    error && console.log(error.message);
 
-// root router, home page
+    const userStr = data.toString();
+    const userArg = userStr.split("\n");
 
-app.get("/", (req, res) =>{
-    console.log("recived request to home router");
+    if (userArg.includes(str)) {
+      res.send("<h1 style='color: green'>Logined successfully</h1>");
+    } else {
+      res.send("<h1 style='color: red'>Invalid Login</h1>");
+    }
+  });
 
-    res.send(`
-    
-    <h1> You are in the homepage<h1>
-    
-    <a href="/register"> <button>Register</button></a>
-    
-    `);
+  //
+  //   res.sendFile(__dirname + "/src/loginForm.html");
+});
+
+// root router , home page
+app.get("/", (req, res) => {
+  console.log("received request to home router");
+
+  res.send(`
+  <h1>You are in the homepage</h1>
+
+ <a href="/register">
+<button>Register</button>
+ </a>
+
+ <a href="/login">
+<button>Login</button>
+ </a>
+
+  `);
 });
 
 // make our server available on http request
 
-app.listen(PORT, error=>{
-    error? console.log(error.message) :
-    console.log(`Server running at http://localhost:${PORT}`)
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error.message)
+    : console.log(`Server running at http://localhost:${PORT}`);
 });
